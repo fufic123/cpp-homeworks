948c42e15ea96afe908e2ebb9e09e8e475f585e8
xDmarkxD <98762887+xDmarkxD@users.noreply.github.com>
2022-05-12 16:12:59 +0300

    Create main.cpp

---
+++ cpp_homework_6/main.cpp
@@ -0,0 +1,72 @@
+//
+//  main.cpp
+//  gwjvnwsbef
+//
+//  Created by Mark Semenov on 05.05.2022.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+//    int s;
+//    float w; // сами понимаете почему такое w а не такое V
+//    int t;
+//    int hour;
+//    int min;
+//    int sec;
+//
+//    cout << "Введите расстояние в метрах: ";
+//    cin >> s;
+//    cout << "Введите скорость в км/час: ";
+//    cin >> w;
+//
+//    w = w/1000*3600;
+//    t = s/w;
+//    hour = t/60/60;
+//    min = (t - (hour*3600))/60;
+//    sec = t - (hour*3600 + min*60);
+//
+//
+//    cout << "Человек был в пути " << hour << " часов, " << min << " минут и " << sec << " секунд\n";
+//
+    
+//    double T;
+//    int kg;
+//    int g;
+//
+//    cout << "Введите дробное число тонн: ";
+//    cin >> T;
+//
+//    kg = T*1000 - (int)T*1000;
+//    g = T*1000*1000 - (int)T*1000*1000 - kg*1000;
+//    cout << (int)T << " тонн " << kg << " кг " << g << " г\n";
+    
+//    int sec, min, hour = 0;
+//      cin >> sec;
+//      min = sec / 60;
+//      hour = sec / 60 / 60;
+//      cout << sec << " секунд = " << min << " минут = " << hour << " час(ов) \n";
+    
+//    double h,m,s,h1,m1,s1, result;
+//    double hr, mr, sr, sum;
+//
+//    cout << "Введите когда начался разговор(часы, секунды и минуты): ";
+//    cin >> h;
+//    cin >> m;
+//    cin >> s;
+//
+//    cout << "Введите когда закончился разковор(часы, секунды и минуты): ";
+//    cin >> h1;
+//    cin >> m1;
+//    cin >> s1;
+//
+//    hr = h1 - h;
+//    mr = (m1 - m)/10;
+//    sr = (s1 - s)/100;
+//
+//    sum = (hr + mr + sr)*60;
+//
+//    result = sum/15;
+//    cout << "С вас " << result << " коп\n";
+}

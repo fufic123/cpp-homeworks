24537d0d4a0334fc54e223b31151a45da57d8b38
Mark Semenov <98762887+fufic123@users.noreply.github.com>
2022-06-02 16:01:41 +0300

    Update main.cpp

--- cpp_homework_11/main.cpp
+++ cpp_homework_11/main.cpp
@@ -63,7 +63,6 @@ int main(int argc, const char * argv[]) {
     c = (a / 100000 % 10) + (a / 10000 % 10) + (a / 1000 % 10);
     b = (a / 100 % 10) + (a / 10 % 10) + (a % 10);
 
-
     if (c == b) {
         cout << "Ok";
     }

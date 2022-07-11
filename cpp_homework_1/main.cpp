4e4fc62d48d646057bf5a6d4a7d8ea472b63e5a2
xDmarkxD <98762887+xDmarkxD@users.noreply.github.com>
2022-07-11 17:57:02 +0200

    Create main.cpp

---
+++ cpp_homework_1/main.cpp
@@ -0,0 +1,55 @@
+//
+//  main.cpp
+//  main.cpp
+//
+//  Created by Mark Semenov on 16.05.2022.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+
+    setlocale(LC_ALL, "English");
+    
+//Task 1
+    
+    cout << "   ----------------------------- ";
+    cout << "  | 10:35:52                    |";
+    cout << "  |                             |";
+    cout << "  | Thursday, 21 April          |";
+    cout << "  |-----------------------------|";
+    cout << "  | April 2022                  |";
+    cout << "  | Mon Tue Wed Thu Fri Sat Sun |";
+    cout << "  |  28  29  30  31   1   2   3 |";
+    cout << "  |  4    5   6   7   8   9  10 |";
+    cout << "  |  11  12  13  14  15  16  17 |";
+    cout << "  |  18  19  20  21  22  23  24 |";
+    cout << "  |  25  26  27  28  29  30   1 |";
+    cout << "  |   2   3   4   5   6   7   8 |";
+    cout << "   ----------------------------- \n";
+    
+//Task 2
+    
+    cout <<"                    Me and my sister                     ";
+    cout <<"                      /      \\                          ";
+    cout <<"                     /        \\                         ";
+    cout <<"                    /          \\                        ";
+    cout <<"               My dad           My mom                   ";
+    cout <<"                / \\              /  \\                  ";
+    cout <<"               /   \\            /    \\                 ";
+    cout <<"              /     \\          /      \\                ";
+    cout <<"  My grandmother  My grandpa My grandpa My grandmother \n";
+    
+//Task 3
+    
+    cout << "      *********    **      **    *********    ***       *******       ";
+    cout << "      *********    **      **    *********    ***      ****    *      ";
+    cout << "      ***          **      **    ***                  ****            ";
+    cout << "      *********    **      **    *********    ***    ****             ";
+    cout << "      *********    **      **    *********    ***    ****             ";
+    cout << "      ***          **      **    ***          ***    ****             ";
+    cout << "      ***          **      **    ***          ***     ****            ";
+    cout << "      ***           **    **     ***          ***      ****    *      ";
+    cout << "      ***             ****       ***          ***       *******       ";
+}

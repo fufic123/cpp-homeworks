21d77d45fbc27d2e6a5b3e4172d60457c83c3be3
xDmarkxD <98762887+xDmarkxD@users.noreply.github.com>
2022-11-29 17:14:54 +0100

    Create main.cpp

---
+++ cpp_homework_26/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  cpp_homework_26
+//
+//  Created by Mark Semenov on 29.11.2022.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cout << "10\n9\n8\n7\n6\n5\n4\n3\n2\n1\n0\n";
+}

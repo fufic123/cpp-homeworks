30b09c24d6d53db1cf2680b3100003d606d795ec
fufic123 <semenovmark07@gmail.com>
2023-05-12 22:18:04 +0200

    Create main.cpp

---
+++ cpp_homework_51/main.cpp
@@ -0,0 +1,31 @@
+#include "MyArray.h"
+
+int main(){
+    srand(time(0));
+    
+    MyArray<int> array(5);
+    MyArray<int> array2(5);
+    MyArray<int> array3;
+    
+    array.Input();
+    array.Print();
+    array2.Input();
+    array2.Print();
+    
+    array3 = array + array2;
+    array3.Print();
+    
+    array3 = array3 - 2;
+    array3.Print();
+    
+    array3 = array3 + 2;
+    array3.Print();
+    
+    array3++;
+    array3.Print();
+    
+    array3--;
+    array3.Print();
+    
+    cout << array3[5] << "\n";
+}

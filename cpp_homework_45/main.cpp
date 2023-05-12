304bfd67a997924af9f4542afa9c630a33691ca7
fufic123 <semenovmark07@gmail.com>
2023-05-12 21:47:20 +0200

    Create main.cpp

---
+++ cpp_homework_45/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Headers/HDD.h"
+#include "Headers/VideoCard.h"
+#include "Headers/CPU.h"
+#include "Headers/RAM.h"
+#include "Headers/Laptop.h"
+using namespace std;
+
+int main() {
+	VideoCard obj1("Nvidia RTX 3060", 12, 30000);
+	CPU obj3("Intel Core i7", 3.9, 12000);
+	RAM obj4("Kingston", 32, 4600);
+	HDD obj2("Asus", 512, 4800);
+	Laptop laptop("MSI", obj1, obj2, obj3, obj4);
+	laptop.Print();
+}

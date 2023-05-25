ab08986152c687f2f6663bbe414ac2ea10ded4fb
fufic123 <semenovmark07@gmail.com>
2023-05-25 09:37:56 +0200

    Create includes.h

---
+++ cpp_homework_62/includes.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <windows.h>
+#include <vector>
+#include "Automobile.h"
+#include "Bike.h"
+#include "Car.h"
+#include "Door.h"
+#include "Engine.h"
+#include "Plane.h"
+#include "Ship.h"
+#include "Truck.h"
+#include "Vehicle.h"
+#include "Wheels.h"

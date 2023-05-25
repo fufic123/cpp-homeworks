70552f6352052ae22b451f5898f7f8f3f3fd053c
fufic123 <semenovmark07@gmail.com>
2023-05-25 09:56:05 +0200

    Update main.cpp

--- cpp_homework_67/main.cpp
+++ cpp_homework_67/main.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
-struct Print { void operator()(int value) const { std::cout << value << " "; } };
+using namespace std;
+struct Print { void operator()(int value) const { cout << value << " "; } };
 
 class Multiply { 
 private:
-	int val;
+	int value1;
 
 public:
-	Multiply(int val) { this->val = val; };
-	void operator()(int& value) { value *= val; } 
+	Multiply(int value1) { this->value1 = value1; };
+	void operator()(int& value2) { value2 *= value1; } 
 };
 
 int main() {
-	std::vector<int> numbers = { 1, 2, 3, 4, 5 };
-	std::for_each(numbers.begin(), numbers.end(), Multiply(3));
-	std::for_each(numbers.begin(), numbers.end(), Print());
+	vector<int> n = { 1, 2, 3, 4, 5 };
+	for_each(n.begin(), n.end(), Multiply(3));
+	for_each(n.begin(), n.end(), Print());
 }

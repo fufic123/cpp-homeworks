8a0d76989647df4b2dc2f1f5d4cfa09306cc46c0
fufic123 <semenovmark07@gmail.com>
2023-05-26 10:11:02 +0200

    Update main.cpp

--- cpp_homework_72/main.cpp
+++ cpp_homework_72/main.cpp
@@ -30,20 +30,17 @@ public:
 int main() {
     std::set<Point<int>> pointSet;
 
-    // Добавление элементов в множество
     pointSet.insert(Point<int>(1, 2));
     pointSet.insert(Point<int>(3, 4));
     pointSet.insert(Point<int>(5, 6));
     pointSet.insert(Point<int>(7, 8));
     pointSet.insert(Point<int>(9, 10));
 
-    // Вывод элементов множества на экран
     std::cout << "Элементы множества Point:\n";
     for (const auto& point : pointSet) {
         std::cout << "X: " << point.getX() << ", Y: " << point.getY() << std::endl;
     }
 
-    // Поиск одного из элементов множества
     Point<int> searchPoint(5, 6);
     auto it = pointSet.find(searchPoint);
     if (it != pointSet.end()) {

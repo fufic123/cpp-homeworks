e8bd9c0109fbe3e577c96e26376a7ab812f2a8d0
fufic123 <semenovmark07@gmail.com>
2023-05-26 10:40:01 +0200

    Update Expense.cpp

--- FMS/Expense.cpp
+++ FMS/Expense.cpp
@@ -1,30 +1,30 @@
 #include "Expense.hpp"
 using namespace std;
 
-Expense::Expense() : ExpenseAmount(0), ECategory(Category()), EDate(Date()), ETime(Time()) {}
-Expense::Expense(double ExpenseAmount, Category ECategory, Date EDate, Time ETime) : 
-	ExpenseAmount(ExpenseAmount), ECategory(ECategory), EDate(EDate), ETime(ETime) {}
-Expense::Expense(double ExpenseAmount, string ExpenseCategoryName, double ExpenseCategoryCashback, unsigned short day, unsigned short month, unsigned short year, unsigned short seconds, unsigned short minutes, unsigned short hours) :
-	ExpenseAmount(ExpenseAmount), ECategory(Category(ExpenseCategoryName, ExpenseCategoryCashback)), EDate(Date(day, month, year)), ETime(Time(seconds, minutes, hours)) {}
+Expense::Expense() : EAmount(0), ECategory(Category()), EDate(Date()), ETime(Time()) {}
+Expense::Expense(double EAmount, Category ECategory, Date EDate, Time ETime) : 
+	EAmount(EAmount), ECategory(ECategory), EDate(EDate), ETime(ETime) {}
+Expense::Expense(double EAmount, string ExpenseCategoryName, double ExpenseCategoryCashback, unsigned short day, unsigned short month, unsigned short year, unsigned short seconds, unsigned short minutes, unsigned short hours) :
+	EAmount(EAmount), ECategory(Category(ExpenseCategoryName, ExpenseCategoryCashback)), EDate(Date(day, month, year)), ETime(Time(seconds, minutes, hours)) {}
 
-double Expense::GetExpenseAmount() const { return ExpenseAmount; }
+double Expense::GetEAmount() const { return EAmount; }
 Category Expense::GetExpenseCategory() const { return ECategory; }
 Date Expense::GetExpenseDate() const { return EDate; }
 Time Expense::GetExpenseTime() const { return ETime; }
 
-string Expense::toString() { return "Expense: \nAmount: " + to_string(ExpenseAmount) + "\nCategory: " + ECategory.toString() + "\nDate: " + EDate.toString() + "\nTime: " + ETime.toString(); }
+string Expense::toString() { return "Expense: \nAmount: " + to_string(EAmount) + "\nCategory: " + ECategory.toString() + "\nDate: " + EDate.toString() + "\nTime: " + ETime.toString(); }
 
-void Expense::SetExpenseAmount(double ExpenseAmount) { this->ExpenseAmount = ExpenseAmount; }
+void Expense::SetEAmount(double EAmount) { this->EAmount = EAmount; }
 void Expense::SetExpenseCategory(Category ECategory) { this->ECategory = ECategory; }
 void Expense::SetExpenseDate(Date EDate) { this->EDate = EDate; }
 void Expense::SetExpenseTime(Time ETime) { this->ETime = ETime; }
 
-bool Expense::operator < (const Expense& e) const { return ExpenseAmount < e.ExpenseAmount; }
-bool Expense::operator > (const Expense& e) const { return ExpenseAmount > e.ExpenseAmount; }
+bool Expense::operator < (const Expense& e) const { return EAmount < e.EAmount; }
+bool Expense::operator > (const Expense& e) const { return EAmount > e.EAmount; }
 
 ostream& operator << (ostream& o, const Expense& e) {
 	o << "Expense: " << "\n"
-		<< "Amount: " << e.ExpenseAmount << "\n"
+		<< "Amount: " << e.EAmount << "\n"
 		<< "Category: " << "\n" << e.ECategory << "\n"
 		<< "Date: " << e.EDate << "\n"
 		<< "Time: " << e.ETime;
@@ -33,7 +33,7 @@ ostream& operator << (ostream& o, const Expense& e) {
 
 istream& operator >> (istream& i, Expense& e) {
 	cout << "New Expense:" << "\n" << "Amount: ";
-	i >> e.ExpenseAmount;
+	i >> e.EAmount;
 	cout << "Category: ";
 	i >> e.ECategory;
 	i >> e.EDate;

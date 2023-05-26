ab1b5805ff75d75dcf8a91c2571d9d9014cfbe9e
fufic123 <semenovmark07@gmail.com>
2023-05-26 10:06:03 +0200

    Create includes.hpp

---
+++ FMS/includes.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <windows.h>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+
+#include "User.hpp"
+#include "Card.hpp"
+#include "Category.hpp"
+#include "Date.hpp"
+#include "Expense.hpp"
+#include "functions.hpp"
+#include "Report.hpp"
+#include "ReportGenerator.hpp"
+#include "Time.hpp"
+#include "Wallet.hpp"

509e237ba4ee03d6320ed10f8ccdbebb8290f8a0
fufic123 <semenovmark07@gmail.com>
2023-05-26 10:06:14 +0200

    Create ReportGenerator.cpp

---
+++ FMS/ReportGenerator.cpp
@@ -0,0 +1,83 @@
+#include "ReportGenerator.hpp"
+#include <algorithm>
+using namespace std;
+
+ReportGenerator::ReportGenerator() {}
+map<Date, vector<Expense>> ReportGenerator::generateDayReport(Date date, vector<Account*>& accounts) 
+{
+	vector<Expense> report;
+	int count = report.size();
+	for (int i = 0; i < accounts.size(); i++) {
+		for (int j = 0; j < accounts[i]->getAllExpenses().size(); j++) {
+			if (accounts[i]->getAllExpenses()[j].GetExpenseDate() == date) {
+				report.push_back(accounts[i]->getAllExpenses()[j]);
+			}
+		}
+	}
+	while (count--) {
+		bool IsSwapped = false;
+		for (int i = 0; i < count; i++) {
+			if (report[i] > report[i + 1]) {
+				swap(report[i], report[i + 1]);
+				IsSwapped = true;
+			}
+		}
+	}
+	reverse(report.begin(), report.end());
+	map<Date, vector<Expense>> resoltReport;
+	resoltReport.insert(make_pair(date, report));
+	return resoltReport;
+}
+
+map<Date, vector<Expense>> ReportGenerator::generateWeekReport(Date date, vector<Account*>& accounts) 
+{
+	vector<Expense> report;
+	int count = report.size();
+	for (int i = 0; i < accounts.size(); i++) {
+		for (int j = 0; j < accounts[i]->getAllExpenses().size(); j++) {
+			if ((accounts[i]->getAllExpenses()[j].GetExpenseDate() > date - 8) || (accounts[i]->getAllExpenses()[j].GetExpenseDate() < date)) {
+				report.push_back(accounts[i]->getAllExpenses()[j]);
+			}
+		}
+	}
+	while (count--) {
+		bool IsSwapped = false;
+		for (int i = 0; i < count; i++) {
+			if (report[i] > report[i + 1]) {
+				swap(report[i], report[i + 1]);
+				IsSwapped = true;
+			}
+		}
+	}
+	reverse(report.begin(), report.end());
+	map<Date, vector<Expense>> resoltReport;
+	resoltReport.insert(make_pair(date, report));
+	return resoltReport;
+}
+
+map<Date, vector<Expense>> ReportGenerator::generateMonthReport(Date date, vector<Account*>& accounts) 
+{
+	vector<Expense> report;
+	int count = report.size();
+	for (int i = 0; i < accounts.size(); i++) {
+		for (int j = 0; j < accounts[i]->getAllExpenses().size(); j++) {
+			if (accounts[i]->getAllExpenses()[j].GetExpenseDate().GetMonth() == date.GetMonth() &&
+				accounts[i]->getAllExpenses()[j].GetExpenseDate().GetYear() == date.GetYear()) {
+				report.push_back(accounts[i]->getAllExpenses()[j]);
+			}
+		}
+	}
+	while (count--) {
+		bool IsSwapped = false;
+		for (int i = 0; i < count; i++) {
+			if (report[i] > report[i + 1]) {
+				swap(report[i], report[i + 1]);
+				IsSwapped = true;
+			}
+		}
+	}
+	reverse(report.begin(), report.end());
+	map<Date, vector<Expense>> resoltReport;
+	resoltReport.insert(make_pair(date, report));
+	return resoltReport;
+}
